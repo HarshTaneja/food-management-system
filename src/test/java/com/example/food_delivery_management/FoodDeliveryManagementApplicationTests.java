2fecdb83de7e909040645e42d4ff3c7f89786d4c
Harsh Taneja <harshtaneja009@gmail.com>
2025-05-17 16:16:14 +0530

    initial commit

---
+++ src/test/java/com/example/food_delivery_management/FoodDeliveryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_delivery_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
